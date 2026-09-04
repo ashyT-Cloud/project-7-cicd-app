622b6c2755824e04c0ad41156e4c13465071fb88
ashyT-Cloud <ashytech.innovate@gmail.com>
2026-09-04 10:03:18 +0000

    feat: release application version 0.2.0

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ def home():
         {
             "application": APP_NAME,
             "version": APP_VERSION,
-            "message": "Project 7 CI/CD application is running",
+            "message": "Project 7 CI/CD application v0.2.0 is running",
         }
     )
 
